3b14670937062e19c92deaa7ee8081cf53eb43cc
Nevac <cagatayoezyurt@gmail.com>
2024-10-16 12:07:04 +0200

    refactor: renamed tabs

--- autofeedback-webapp/src/domains/chats/chat-details/chat-detail.component.tsx
+++ autofeedback-webapp/src/domains/chats/chat-details/chat-detail.component.tsx
@@ -44,19 +44,19 @@ export default function ChatDetailComponent() {
             <Box sx={{ width: '100%' }}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab label="Item One" {...a11yProps(0)} />
-                        <Tab label="Item Two" {...a11yProps(1)} />
-                        <Tab label="Item Three" {...a11yProps(2)} />
+                        <Tab label="ChatGPT" {...a11yProps(0)} />
+                        <Tab label="Gemini" {...a11yProps(1)} />
+                        <Tab label="Claude" {...a11yProps(2)} />
                     </Tabs>
                 </Box>
                 <CustomTabPanel value={value} index={0}>
-                    Item One
+                    ChatGPT
                 </CustomTabPanel>
                 <CustomTabPanel value={value} index={1}>
-                    Item Two
+                    Gemini
                 </CustomTabPanel>
                 <CustomTabPanel value={value} index={2}>
-                    Item Three
+                    Claude
                 </CustomTabPanel>
             </Box>
         </Paper>
